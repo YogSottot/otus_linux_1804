230a20eb564512a6fd2d18ada5e8634b620ea250
YogSottot <yogsottot@boson.nom.za>
2018-05-09 18:08:30 +0300

    HW5 prevent duplicate script

---
+++ 1/05/prevent_duplicate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PIDFILE=/var/www/test.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+/usr/bin/php -f /var/www/domain.tld/exports/test_h.php
+rm $PIDFILE

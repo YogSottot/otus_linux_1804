25048e7845cdd3130b76a1a89fdd6c132bb85591
YogSottot <yogsottot@boson.nom.za>
2018-10-31 23:57:52 +0300

    Add xtrabackup script

--- 5.Project/provisioning/roles/wordpress/templates/wp-config.php
+++ 5.Project/provisioning/roles/wordpress/templates/wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', '{{ wp_db_user }}');
 define('DB_PASSWORD', '{{ wp_db_password }}');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
